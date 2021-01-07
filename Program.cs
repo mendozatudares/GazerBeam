bb883a59108041940d79404ac88dfb7766ca435a
Kevin Mendoza Tudares <mendozatudares@u.northwestern.edu>
2021-01-07 16:39:40 -0600

    update Program.cs with persistent logging mode

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 using Tobii.Interaction;
 
@@ -12,6 +13,7 @@ namespace Interaction_Streams_101
         private static Host _host;
         private static bool _stopOnDwell;
         private static bool _moveOnDwell;
+        private static bool _loggingMode;
         private static bool _wKeyUp;
 
         // Windows API helper functions
@@ -49,6 +51,7 @@ namespace Interaction_Streams_101
             var collect = new Stopwatch();
             var dwell = new Stopwatch();
             bool moved;
+            Point prevEyePos = new Point(-1, -1);
 
             if (args.Length > 0)
             {
@@ -56,12 +59,17 @@ namespace Interaction_Streams_101
                     _stopOnDwell = true;
                 else if (args[0].Equals("-m"))
                     _moveOnDwell = true;
+                else if (args[0].Equals("-l"))
+                    _loggingMode = true;
             }
 
             // Get the currently running MultiCraft window. If no window found, end the program.
             var minecraftWindow = FindWindow(null, "Minecraft 1.9");
             if (!GetWindowRect(minecraftWindow, out var rect))
+            {
+                Console.WriteLine("No Minecraft 1.9 window found.");
                 EndHostConnection();
+            }
             var windowWidth = rect.Right - rect.Left;
             var windowHeight = rect.Bottom - rect.Top;
 
@@ -72,14 +80,17 @@ namespace Interaction_Streams_101
             // Get the gaze data.  
             gazePointDataStream.GazePoint((x, y, _) =>
             {
-                var eyePos = new Point((int)x, (int)y);
-
                 if (dwell.IsRunning && dwell.Elapsed > TimeSpan.FromSeconds(3))
                 {
+                    Console.Write("[{0}]: dwell exceeded 3 seconds.");
                     if (_stopOnDwell)
+                    {
+                        Console.WriteLine("terminating.");
                         EndHostConnection();
+                    }
                     else if (_moveOnDwell && _wKeyUp)
                     {
+                        Console.WriteLine("moving forward.");
                         _wKeyUp = false;
                         KeyboardEvent(VK_W, 0, KEYEVENTF_EXTENDEDKEY, 0);
                     }
@@ -87,13 +98,21 @@ namespace Interaction_Streams_101
 
                 if (collect.Elapsed > TimeSpan.FromMilliseconds(16))
                 {
-                    if (GetCursorPos(out var cursorPos) && ScreenToClient(minecraftWindow, ref eyePos))
+                    var t = DateTime.UtcNow.ToString("hh:mm:ss.fff");
+                    Console.WriteLine("[{0}]: x: {1}, y: {2}", t, (int)x, (int)y);
+                    var eyePos = new Point((int)x, (int)y);
+
+                    if (!_loggingMode && GetCursorPos(out var cursorPos) && ScreenToClient(minecraftWindow, ref eyePos))
                     {
                         moved = MoveCursor(eyePos, cursorPos, windowWidth, windowHeight);
                         if (!moved && !dwell.IsRunning)
+                        {
                             dwell.Start();
+                            Console.WriteLine("[{0}]: starting dwell.", t);
+                        }
                         else if (moved && dwell.IsRunning)
                         {
+                            Console.WriteLine("[{0}]: ending dwell. length: {1} ms", t, dwell.Elapsed.TotalMilliseconds);
                             dwell.Reset();
                             if (_moveOnDwell && !_wKeyUp)
                             {
@@ -102,16 +121,54 @@ namespace Interaction_Streams_101
                             }
                         }
                     }
+                    else if (_loggingMode && prevEyePos.X != -1 && prevEyePos.Y != -1)
+                    {
+                        moved = CheckDisplacement(eyePos, prevEyePos, windowWidth, windowHeight);
+                        if (!moved && !dwell.IsRunning)
+                        {
+                            dwell.Start();
+                            Console.WriteLine("[{0}]: starting dwell.", t);
+                        }
+                        else if (moved && dwell.IsRunning)
+                        {
+                            Console.WriteLine("[{0}]: ending dwell. length: {1} ms", t, dwell.Elapsed.TotalMilliseconds);
+                            dwell.Reset();
+                        }
+                    }
+                    prevEyePos.X = eyePos.X;
+                    prevEyePos.Y = eyePos.Y;
+
                     collect.Reset();
                     collect.Start();
                 }
             });
 
-            while (GetKeyState((int) Keys.OemPeriod) == 0)
+            while (true)
             {
                 // Wait for user to press . key to end.
+                Thread.Sleep(16);
+                
+                if (!_loggingMode && GetKeyState((int)Keys.OemPeriod) == 1)
+                    break;
+                if (_loggingMode && Console.KeyAvailable && Console.ReadKey(true).KeyChar.Equals('.'))
+                    break;
             }
             EndHostConnection();
+        }
+
+        private static bool CheckDisplacement(Point eyePos, Point prevEyePos, int windowWidth, int windowHeight)
+        {
+            // Get displacement from previous eye position
+            var displaceX = eyePos.X - prevEyePos.X;
+            var displaceY = eyePos.Y - prevEyePos.Y;
+
+            // Set up bounding box for dwell
+            if (Math.Abs(displaceX) < 25 && Math.Abs(displaceY) < 25)
+                return false;
+
+            // Return true if eye position has significant displacement
+            return true;
+
         }
 
         private static bool MoveCursor(Point eyePos, Point cursorPos,  int windowWidth, int windowHeight)
@@ -127,7 +184,6 @@ namespace Interaction_Streams_101
             // If eyePos is within bounding box, don't move cursor
             if (displaceX == 0 && displaceY == 0)
                 return false;
-
             SetCursorPos(cursorPos.X + displaceX / 40, cursorPos.Y + displaceY / 30);
             return true;
         }
